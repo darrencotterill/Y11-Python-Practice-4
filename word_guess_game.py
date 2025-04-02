2a7207bc4572a6ccd1cb4f3a06f0ac8773da46da
darrencotterill <darren.cotterill2@det.nsw.edu.au>
2025-04-02 22:36:12 +0000

    start of word guessing game

---
+++ word_guess_game.py
@@ -0,0 +1,41 @@
+from testhelper import test
+import requests
+import random
+import time
+
+url = "https://raw.githubusercontent.com/dwyl/english-words/refs/heads/master/words_alpha.txt"
+
+# Get a list of all english words
+words_list = requests.get(url).content.split()
+all_words = []
+nine_letter_words = []
+
+#Select the words that are >2 letters and all the nine letter words
+for word in words_list:
+    if len(word) > 2 and len(word) < 10:
+        all_words.append(word.decode('ascii'))
+    if len(word) == 9:
+        nine_letter_words.append(word)
+
+word_to_guess = nine_letter_words[random.randint(0,len(nine_letter_words))]
+
+start_time = time.time()
+score = 0
+
+def draw_word_grid():
+    # You need to write this function
+    pass
+
+while (time.time() < (start_time + 60)):
+    draw_word_grid()
+    # Replace the code below with your code 
+    word = input("Guess:")
+    print(word)
+
+print ("You scored: " + str(score))    
+
+
+
+
+    
+    
